9a8e2a392c9122a2b5e71f15f90ac0518c3a05d4
Rahul Kumar <rahulrai2822march@gmail.com>
2025-09-06 00:00:02 +0530

    grid lights

--- src/components/grid-lights/GridLights.jsx
+++ src/components/grid-lights/GridLights.jsx
@@ -6,8 +6,20 @@ const GridLights = () => {
     [0, 1, 1],
     [1, 0, 1],
   ];
-  
-  return <div>GridLights</div>;
+
+  return (
+    <div className="grid-light-box">
+      {config?.map((row, i) => (
+        <div key={i}>
+          {row?.map((cube, j) => {
+            <div key={j} className="light-cube">
+              {cube}
+            </div>;
+          })}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default GridLights;
