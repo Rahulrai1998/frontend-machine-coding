f5df0c2fc91b839690c0392c89a5f3f9ebc561e4
Rahul Kumar <rahulrai2822march@gmail.com>
2025-09-07 17:53:04 +0530

    updates

--- src/components/grid-lights/GridLights.jsx
+++ src/components/grid-lights/GridLights.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const GridLights = () => {
+  const [stack, setStack] = useState({});
   const config = [
     [1, 0, 1],
     [0, 1, 1],
     [1, 0, 1],
   ];
+
+  const handleClick = (rowIndex, colIndex) => {
+    const newStack = structuredClone(stack);
+    const key = `${rowIndex}-${colIndex}`;
+    if (newStack[key]) {
+      return;
+    } else {
+      newStack[key] = true;
+    }
+    setStack(newStack);
+    console.log(stack);
+  };
 
   return (
     <div className="grid-light-box">
@@ -14,8 +27,11 @@ const GridLights = () => {
           {row?.map((cube, j) => {
             return (
               <div
+                onClick={() => cube && handleClick(i, j)}
                 key={j}
-                className={`light-cube ${cube === 0 ? "off-bg" : null}`}
+                className={`light-cube ${cube === 0 ? "off-bg" : null} ${
+                  stack[`${i}-${j}`] ? "on-bg" : null
+                }`}
               ></div>
             );
           })}
