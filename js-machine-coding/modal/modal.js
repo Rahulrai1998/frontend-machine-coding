9f01af510bdae82d9575863a899b32d63b1048ef
RahulTechanzo <rahulrai2822march@gmail.com>
2026-02-07 15:49:04 +0530

    js modal

--- js-machine-coding/modal/modal.js
+++ js-machine-coding/modal/modal.js
@@ -3,7 +3,30 @@
 - while the modal is open the backgroun should be blur
 - while click on close or outside the modal the modal should get closed.
 - on clicking inside the modal the modal should not get closed.
+
 */
 
-const parentDiv = document.getElementById("parent");
-const modalDiv = document.getElementById("modal");
+const parentDiv = document.querySelector(".parent");
+const modalDiv = document.querySelector(".modal");
+const closeModal = document.querySelector(".close-modal");
+const modalBtn = document.querySelector(".open-modal");
+
+function toggle(open) {
+  parentDiv.style.display = open ? "flex" : "none";
+}
+
+modalBtn.addEventListener("click", () => {
+  toggle(true);
+});
+
+parentDiv.addEventListener("click", (event) => {
+  if (event.target.className === "parent") toggle(false);
+});
+
+closeModal.addEventListener("click", () => toggle(false));
+
+//1st approach to close modal on clicking outside the modal
+// modalDiv.addEventListener("click", (event) => {
+//   console.log(event);
+//   event.stopPropagation();
+// });
