d64acc8eb142c85cfda61846c7c2bc6ab4e18111
RahulTechanzo <rahulrai2822march@gmail.com>
2026-02-11 06:59:40 +0530

    updates

--- src/components/modal-one/components/Modal.jsx
+++ src/components/modal-one/components/Modal.jsx
@@ -15,7 +15,6 @@ const Modal = ({ open, handleModal }) => {
     //"mousedown" event trigger before "click" evnent
     //hence, the modal is opening on button click.
     document.addEventListener("mousedown", cb);
-
     return () => {
       document.removeEventListener("mousedown", cb);
     };
@@ -30,6 +29,14 @@ const Modal = ({ open, handleModal }) => {
         since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book. It has survived not only five
         centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry. Lorem Ipsum has been the industry's standard dummy
+        text ever since the 1500s, when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book. It has survived not only
+        five centuries, but also the leap into electronic typesetting, remaining
         essentially unchanged. It was popularised in the 1960s with the release
         of Letraset sheets containing Lorem Ipsum passages, and more recently
         with desktop publishing software like Aldus PageMaker including versions
