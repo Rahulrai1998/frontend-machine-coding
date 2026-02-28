1c17b64740099d5a3abfee6c455b310dde4429d8
RahulTechanzo <rahulrai2822march@gmail.com>
2026-02-28 07:43:21 +0530

    updates

--- js-coding/MostAsked/flatten.js
+++ js-coding/MostAsked/flatten.js
@@ -9,6 +9,7 @@
 
 // Recursive approach - 1, without depth
 //using reduce and concat methods.
+//reduce() method is relatively slower than for-loop
 function flattenArray(arr) {
   if (!Array.isArray(arr)) throw new Error("Invalid input");
   return arr.reduce((acc, cur) => {
@@ -20,6 +21,7 @@ let testCase = [[[[[0]], [1]], [[[2], [3]]], [[4], [5]]]];
 console.log(flattenArray(testCase));
 
 //Recursive approach - 2, without depth
+//this approach is much faster than reduce()
 const flattenArray2 = (arr) => {
   if (!Array.isArray(arr)) throw new Error("Input must be an array!!");
   const result = [];
@@ -30,6 +32,38 @@ const flattenArray2 = (arr) => {
   return result;
 };
 
-console.log(flattenArray2([[[[[0]], [1]], [[[2], [3]]], [[4], [5]]]]))
+console.log(flattenArray2([[[[[0]], [1]], [[[2], [3]]], [[4], [5]]]]));
 
 //Iterative approach, without depth
+const flattenArray3 = (arr) => {
+  if (!Array.isArray(arr)) throw new Errro("Input must be an array");
+  let stack = [...arr];
+  let result = [];
+  while (stack.length) {
+    let elm = stack.pop();
+    if (Array.isArray(elm)) stack.push(...elm);
+    else result.push(elm);
+    // else result.unshift(elm); //unshift():O(N) ,  push():O(1)
+  }
+  result.reverse(); //reverse(): O(N)
+  return result;
+};
+console.log(flattenArray3([[[[[0]], [1]], [[[2], [3]]], [[4], [5]]]]));
+
+//follow up, flatten the array with given depth.
+//recursive approach, with depth.
+
+const flattenArray4 = (arr, depth) => {
+  if (!Array.isArray(arr)) throw new Error("Input must be array");
+  if (depth === 0) return arr;
+
+  let result = [];
+  for (let elm of arr) {
+    if (Array.isArray(elm) && depth > 0)
+      result.push(...flattenArray4(elm, depth - 1));
+    else result.push(elm);
+  }
+  return result;
+};
+console.log(flattenArray4([[[[[0]], [1]], [[[2], [3]]], [[4], [5]]]], 1));
+console.log(flattenArray4([1, [2], [[3]]] , 2));
