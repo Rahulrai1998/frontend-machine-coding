553cd5fe2fb973e689b3f1550e270fea828ad982
RahulTechanzo <rahulrai2822march@gmail.com>
2026-03-07 08:03:20 +0530

    updates

--- src/components/debounce/useDebounce.js
+++ src/components/debounce/useDebounce.js
@@ -10,6 +10,10 @@ const useDebounce = (value, delay) => {
         setDebouncedValue(value);
       }, delay);
     }
+    //the cleanup function will run before the new effect executed due to dependencies change.
+    //The cleanup function runs in two scenarios:
+    //1. when dependencies change and new effect executes.
+    //2. when component unmounts.
     return () => {
       clearTimeout(timer);
     };
