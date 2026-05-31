1d827e597ce969c4023e0c4f5251da2d03ec4b1e
RahulTechanzo <rahulrai2822march@gmail.com>
2026-05-31 09:51:23 +0530

    updates

--- src/components/redux-counter/index.jsx
+++ src/components/redux-counter/index.jsx
@@ -1,27 +1,46 @@
 import React, { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Privacy from "./Privacy";
+import { counterActions } from "../../store";
 
 const ReduxCounter = () => {
   const inputRef = useRef(0);
-  const counter = useSelector((store) => store.counter);
+  // const counter = useSelector((store) => store.counter); REDUX
+  const counter = useSelector((store) => store.counter.count);
   const status = useSelector((store) => store.showCounter);
   const dispatch = useDispatch();
   return (
     <div>
       <h3>Counter</h3>
-      <input type="number" placeholder="enter..." ref={inputRef} />{" "}
+      <input type="number" placeholder="enter..." ref={inputRef} /> {counter}
       <button
         onClick={() =>
-          dispatch({ type: "ADD", payload: { data: inputRef.current.value } })
+          // dispatch({ type: "ADD", payload: { data: inputRef.current.value } }) REDUX
+          dispatch(
+            counterActions.add({
+              num: Number(inputRef.current.value),
+            }),
+          )
         }
       >
         Add
       </button>
       <h4>{status ? counter : ""}</h4>
       <Privacy />
-      <button onClick={() => dispatch({ type: "INCREMENT" })}>Increase</button>
-      <button onClick={() => dispatch({ type: "DECREMENT" })}>Decrease</button>
+      <button
+        onClick={() => {
+          dispatch(counterActions.increment());
+          console.log(counterActions.increment());
+        }}
+      >
+        Increase
+      </button>
+      <button onClick={() => dispatch(counterActions.decrement())}>
+        Decrease
+      </button>
+      {/*REDUX*/}
+      {/* <button onClick={() => dispatch({ type: "INCREMENT" })}>Increase</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>Decrease</button> */}
     </div>
   );
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,50 @@
-import { createStore } from "redux";
+// import { createStore } from "redux"; REDUX
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const INITIAL_VALUE = { counter: 0, showCounter: true };
+// const INITIAL_VALUE = { counter: 0, showCounter: true }; REDUX
 
-const counterReducer = (store = INITIAL_VALUE, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return { ...store, counter: store.counter + 1 };
-    case "DECREMENT":
-      return { ...store, counter: store.counter - 1 };
-    case "ADD":
-      return { ...store, counter: store.counter + Number(action.payload.data) };
-    case "SHOW-HIDE":
-      return { ...store, showCounter: !store.showCounter };
-    default:
-      return store;
-  }
-  return store;
-};
+//COUNTER SLICE
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { count: 0 },
+  reducers: {
+    increment: (state) => {
+      state.count++;
+    },
+    decrement: (state) => {
+      state.count--;
+    },
+    add: (state, action) => {
+      state.count += action.payload.num;
+    },
+  },
+});
 
-const store = createStore(counterReducer);
+//PRIVACY SLICE
+
+//REDUX
+// const counterReducer = (store = INITIAL_VALUE, action) => {
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return { ...store, counter: store.counter + 1 };
+//     case "DECREMENT":
+//       return { ...store, counter: store.counter - 1 };
+//     case "ADD":
+//       return { ...store, counter: store.counter + Number(action.payload.data) };
+//     case "SHOW-HIDE":
+//       return { ...store, showCounter: !store.showCounter };
+//     default:
+//       return store;
+//   }
+//   return store;
+// };
+
+// const store = createStore(counterReducer); REDUX
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+  },
+});
+
+export const counterActions = counterSlice.actions;
 export default store;
